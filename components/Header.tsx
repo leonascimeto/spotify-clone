b322f17c3fb992c8a494d44ffd0e9eacdc5f5896
Leo <nascimentoleo899@gmail.com>
2023-06-24 11:41:52 -0300

    feat: finish base layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
    children: React.ReactNode
@@ -47,7 +48,28 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
                   <BiSearch className="text-black" size={20} />
                </button>
             </div>
+            <div className="flex justify-between items-center gap-x-4">
+               <>
+                  <div>
+                     <Button
+                        onClick={() => {}}  
+                        className="bg-transparent text-neutral-300 font-medium"
+                     >
+                        Sign up
+                     </Button>
+                  </div>
+                  <div>
+                     <Button
+                        onClick={() => {}}  
+                        className="bg-white px-6 py-2"
+                     >
+                        Login
+                     </Button>
+                  </div>
+               </>
+            </div>
          </div>
+         {children}
       </div>
    )
 }
